c284df866f4dd993384cabcb4ad5d3ae3335141b
Aiman Yusra <aimanyusra@hotmail.com>
2017-07-04 23:57:13 +0800

    added capybara and user model tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
-describe User, 'validations' do
-  it { should validate_presence_of(:name) }
-end
+ RSpec.describe User, type: :model do
+    it { should validate_length_of(:name).is_at_most(50) }
+    it { should validate_length_of(:password).is_at_least(6) }
+    it { should have_secure_password }
+    it { should validate_presence_of(:name) }
+    it { should validate_presence_of(:email) }
+    it { should validate_presence_of(:password) }
+    it { should have_many(:photos) }
+  end
